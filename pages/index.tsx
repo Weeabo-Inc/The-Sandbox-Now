157229dffaf29ae49966b146e68249a636b51ed5
Edge <48862659+Shigetorum635@users.noreply.github.com>
2022-09-23 13:54:13 +0200

    feat: my best description ever
    Im crying

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,11 @@ const Home: NextPage = () => {
         <div>News</div>
       </div>
 
-      <div className=' text-center xl:w-2/4 mx-auto text-gray-400  font-semibold text-lg'>Does <span>BrickPlanet</span> respect your data, your transparency requests or your privacy rights? This web app allows you to see if they keep up to their promises. With the 8th release of BrickPlanet around the corner, safety is never enough. Many refunds from the 7th iteration have never reached the users, don&apos;t let it happen again.</div>
+      <div className=' text-center xl:w-2/4 mx-auto text-gray-400  font-semibold text-lg'>We work on centralizing an uniting sandboxes torwards a common objective; self-improvement, safety and establishment. We write news about the virtual space that's constantly evolving, and watch as it slowly evolves.</div>
       <div className='grid grid-cols-2 xl:w-1/6 mx-auto items-center gap-4 justify-center'>
         <Link href="/news">
           <button className='mt-5 bg-blue-600 rounded w-[100px] mx-auto text-center p-2 text-white font-semibold'>
-            Sandbox News
+            News
           </button>
         </Link>
         <Link href="https://discord.gg/87jwQT38zN">
